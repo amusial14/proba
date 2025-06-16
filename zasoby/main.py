18568396c7d76bb1febdd51386bcbc65658e1ac3
amusial14 <287391@student.pwr.edu.pl>
2025-06-17 00:50:56 +0200

    Update main.py

--- zasoby/main.py
+++ zasoby/main.py
@@ -126,13 +126,13 @@ class Gra:
                             print("Przedmiot podniesiony!")
                 elif zdarz.key == pg.K_m: 
                     self.stan_gry = "mapa"
-           elif zdarz.type == pg.USEREVENT:
+            elif zdarz.type == pg.USEREVENT:
     # Resetowanie licznika obrażeń po immunitecie
-            for pies in self.mapy[self.aktualna_mapa].psy:
-                pies.calkowite_obrazenia = 0
+                for pies in self.mapy[self.aktualna_mapa].psy:
+                    pies.calkowite_obrazenia = 0
     # Przywróć normalny wygląd gracza
-            self.gracz.obraz = pg.image.load("spritey/parszywek1.png").convert_alpha()
-            self.gracz.obraz = pg.transform.scale(self.gracz.obraz, (70, 90))
+                self.gracz.obraz = pg.image.load("spritey/parszywek1.png").convert_alpha()
+                self.gracz.obraz = pg.transform.scale(self.gracz.obraz, (70, 90))
 
     
     def rysuj(self):
